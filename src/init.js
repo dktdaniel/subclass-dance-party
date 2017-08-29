9f94939303a2970ba9830ef409eebc87b542abee
Hack Reactor Student <student@hackreactor.com>
2017-08-28 19:46:42 -0700

    tests passed

--- src/init.js
+++ src/init.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
+    console.log(dancerMakerFunctionName);
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
+      Math.random() * 3000
     );
     $('body').append(dancer.$node);
   });
